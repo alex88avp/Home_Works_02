61dc51d275514ca88d1aef460b2f34db87c7c8c5
aleksander <petrov_records@mail.ru>
2022-10-20 10:50:18 +0400

    mod task0202

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
 Console.Clear();
 
-//Console.Write("Введите число от 1 до 7: ");
-//int number = int.Parse(Console.ReadLine() ?? "0");
 int number = new Random().Next(1, 7);
 if(number <= 5)
 {
