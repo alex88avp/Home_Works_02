f4d01b5104feeb40af1956b9ba5d9f8e5295beb5
aleksander <petrov_records@mail.ru>
2022-10-20 14:24:29 +0400

    task03_1

---
+++ task03_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int q = int.Parse(Console.ReadLine());
+
+if(q == 1)
+{
+    Console.WriteLine("x > 0 и у > 0");
+}
+if(q == 2)
+{
+    Console.WriteLine("x < 0 и у > 0");
+}
+if(q == 3)
+{
+    Console.WriteLine("x < 0 и у < 0");
+}
+if(q == 4)
+{
+    Console.WriteLine("x > 0 и у < 0");
+}
